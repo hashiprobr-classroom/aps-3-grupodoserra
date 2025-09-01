a1533162a66c1bbdfb1b27cdebc185d5ac70701a
Correia1500 <gabrielcc3@al.insper.edu.br>
2025-08-31 23:37:59 -0300

    Classico Cupido meio pronto

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Cupido.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Cupido.java
@@ -1,4 +1,30 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Cupido {
+    private Map<Integer, List<Integer>> likes;
+
+    public Cupido() {
+        this.likes = new HashMap<>();
+    }
+
+    public void registraLike(Integer id1,Integer id2){
+        List<Integer> lista = likes.get(id1);
+        if(!lista.contains(id2)){
+            lista.add(id2);
+        }
+    }
+
+    public boolean match(Usuario u1, Usuario u2){
+        int id1 = u1.getIdentificador();
+        int id2 = u2.getIdentificador();
+
+        boolean u1Curtiuu2 = likes.containsKey(id1) && likes.get(id1).contains(id2);
+        boolean u2Curtiuu1 = likes.containsKey(id2) && likes.get(id2).contains(id1);
+
+        return u1Curtiuu2 && u2Curtiuu1;
+    }
 }
