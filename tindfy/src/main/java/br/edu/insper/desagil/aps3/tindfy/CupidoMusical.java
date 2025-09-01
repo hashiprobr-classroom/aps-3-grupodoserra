d4c6f9fb60d61d99bb474586f2c7a51fb544ef0b
Correia1500 <gabrielcc3@al.insper.edu.br>
2025-08-31 23:43:34 -0300

    cupidoMusical maisoumenos

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/CupidoMusical.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/CupidoMusical.java
@@ -1,4 +1,17 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
-public class CupidoMusical {
+public class CupidoMusical extends Cupido{
+
+    @Override
+    public boolean match(Usuario u1, Usuario u2) {
+        if (!super.match(u1, u2)){
+            return false;
+        }
+        for (Musica m : u1.getMusicas()){
+            if (u2.temMusica(m)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
