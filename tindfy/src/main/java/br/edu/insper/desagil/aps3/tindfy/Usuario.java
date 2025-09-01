9b5a6434345035e6877239bea13620d5e1abb96f
Correia1500 <gabrielcc3@al.insper.edu.br>
2025-08-31 23:16:27 -0300

    Usuario Pronto

--- tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
+++ tindfy/src/main/java/br/edu/insper/desagil/aps3/tindfy/Usuario.java
@@ -1,4 +1,33 @@
 package br.edu.insper.desagil.aps3.tindfy;
 
-public class Usuario {
+import java.util.List;
+
+public class Usuario extends Pessoa{
+    private List<Musica> musicas;
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public Usuario(int identificador, String nome, List<Musica> musicas) {
+        super(identificador, nome);
+        this.musicas = musicas;
+    }
+
+    public boolean temMusica(Musica musica){
+        for (Musica m: musicas) {
+            boolean mesmoArtista = m.getArtista().getIdentificador() == musica.getArtista().getIdentificador();
+            boolean mesmoTitulo = m.getTitulo().equals(musica.getTitulo());
+
+            if (mesmoArtista && mesmoTitulo){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void adicionaMusica(Musica musica) {
+        if (!temMusica(musica)) {
+            musicas.add(musica);
+        }
+    }
 }
